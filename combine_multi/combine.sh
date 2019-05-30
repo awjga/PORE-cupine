f7978f58cc0dd53b026e25f6ff68d3270ee1fce5
awjga <30524842+awjga@users.noreply.github.com>
2019-05-30 10:59:08 +0800

    Update combine.sh

--- combine_multi/combine.sh
+++ combine_multi/combine.sh
@@ -3,6 +3,6 @@ ls -1 */*.tmp | awk -F '/' '{print $2}' | sort | uniq > names
 mkdir combined
 cd combined
 for i in `cat ../names` ; do
-    cat ../header ../*/$i > $i.combined
+    cat ../header ../*/$i > $i
 done
 echo done
